fd9240c664bcb5c40cfd5f7b230675a428809f34
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2024-12-18 10:05:22 -0300

    Contexto de banco de dados adicionado

---
+++ GerenciadorDeLivros/Data/LivroContext.cs
@@ -0,0 +1,11 @@
+﻿using GerenciadorDeLivros.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeLivros.Data;
+
+public class LivroContext : DbContext
+{
+	public LivroContext(DbContextOptions<LivroContext> options) : base(options) { }
+
+	public DbSet<Livro> Livros { get; set; }
+}
