c20ddbee411164703fc4f0b82af3dc4ee4311201
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2024-12-19 10:11:10 -0300

    Serviço adicionado

---
+++ GerenciadorDeLivros/Service/LivroService.cs
@@ -0,0 +1,31 @@
+﻿using GerenciadorDeLivros.Data;
+using GerenciadorDeLivros.Model;
+
+namespace GerenciadorDeLivros.Service;
+
+public static class LivroService
+{
+	static LivroService() { }
+
+	public static List<Livro> GetLivros(LivroContext livroContext) => livroContext.Livros.ToList();
+
+	public static Livro? GetLivro(int id, LivroContext livroContext) => livroContext.Livros.FirstOrDefault(l => l.Id == id);
+
+	public static void AddLivro(Livro livro, LivroContext livroContext)
+	{
+		livroContext.Livros.Add(livro);
+		livroContext.SaveChanges();
+	}
+
+	public static void DeleteLivro(Livro livro, LivroContext livroContext)
+	{
+		livroContext.Livros.Remove(livro);
+		livroContext.SaveChanges();
+	}
+
+	public static void UpdateLivro(Livro livroToUp, LivroContext livroContext)
+	{
+		livroContext.Livros.Update(livroToUp);
+		livroContext.SaveChanges();
+	}
+}
