1323b158af76082bbed6c62d716239ac912ac808
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2024-12-24 09:55:50 -0300

    Migração inicial (tabela) adicionada

---
+++ GerenciadorDeLivros/Migrations/20241224124759_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GerenciadorDeLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "livros",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    titulo = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    autor = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    genero = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    data_de_publicacao = table.Column<DateOnly>(type: "date", nullable: false),
+                    resumo = table.Column<string>(type: "text", nullable: true),
+                    classificacao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_livros", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "livros");
+        }
+    }
+}
