1bc1e5a11be5c3ecd8b3df4b896cc7810f7e0d25
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2025-01-07 10:34:06 -0300

    Repositório de usuário adicionado

---
+++ GerenciadorDeLivros/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using GerenciadorDeLivros.Model;
+
+namespace GerenciadorDeLivros.Repositories;
+
+public class UserRepository
+{
+	/*
+	 * 1 - Cliente
+	 * 2 - Funcionário
+	 * 3 - Gerente
+	 */
+
+	public static IList<User> Users = new List<User>
+	{
+		new()
+		{
+			Type = 3,
+			Name = "Matt",
+			Password = "matt123"
+		},
+		new()
+		{
+			Type = 1,
+			Name = "Jorge",
+			Password = "jorge123"
+		},
+		new()
+		{
+			Type = 2,
+			Name = "John",
+			Password = "john123"
+		}
+	};
+
+	public static User? GetByName(string userName) => Users.Where(u => u.Name == userName).FirstOrDefault();
+}
