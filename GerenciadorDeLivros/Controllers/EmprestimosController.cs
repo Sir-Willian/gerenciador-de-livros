d06432aaa62a3cf87520c11d421d8f353e95a707
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2025-01-20 10:57:52 -0300

    Controlador para empréstimos adicionado

---
+++ GerenciadorDeLivros/Controllers/EmprestimosController.cs
@@ -0,0 +1,60 @@
+﻿using GerenciadorDeLivros.Data;
+using GerenciadorDeLivros.Model;
+using GerenciadorDeLivros.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeLivros.Controllers;
+
+[ApiController()]
+[Route("[controller]")]
+public class EmprestimosController : ControllerBase
+{
+	private readonly EmprestimoContext context;
+
+	public EmprestimosController(EmprestimoContext _context) { context = _context; }
+
+	[HttpGet()]
+	public ActionResult<List<Emprestimo>> GetAll() => EmprestimoService.GetEmprestimos(context);
+
+	[HttpGet("{id}")]
+	public ActionResult<Emprestimo> Get(int id)
+	{
+		var emprestimo = EmprestimoService.GetEmprestimo(id, context);
+		if (emprestimo == null) { return NotFound("Este empréstimo não existe."); }
+
+		return emprestimo;
+	}
+
+	[HttpPost()]
+	public ActionResult<Emprestimo> Post(Emprestimo emprestimo)
+	{
+		var existingEmprestimo = EmprestimoService.GetEmprestimo(emprestimo.EmprestimoId, context);
+		if (existingEmprestimo != null) { return BadRequest("Um empréstimo com este mesmo Id já existe."); }
+
+		EmprestimoService.AddEmprestimo(emprestimo, context);
+
+		return CreatedAtAction(nameof(Get), new { id = emprestimo.EmprestimoId }, emprestimo);
+	}
+
+	[HttpDelete("{id}")]
+	public ActionResult Delete(int id)
+	{
+		var existingEmprestimo = EmprestimoService.GetEmprestimo(id, context);
+		if (existingEmprestimo == null) { return NotFound("Este empréstimo não existe."); }
+
+		EmprestimoService.DeleteEmprestimo(existingEmprestimo, context);
+
+		return NoContent();
+	}
+
+	[HttpPut("{id}")]
+	public ActionResult Put(int id, Emprestimo emprestimo)
+	{
+		var existingEmprestimo = EmprestimoService.GetEmprestimo(id, context);
+		if(existingEmprestimo == null) { return BadRequest("Este empréstimo não existe."); }
+
+		EmprestimoService.UpdateEmprestimo(existingEmprestimo, emprestimo, context);
+
+		return NoContent();
+	}
+}
